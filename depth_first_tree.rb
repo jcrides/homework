f149a178bce848f48f2279cd6f1f1da6f6f0f15a
Jeremy Carey <jeremycar@gmail.com>
2015-04-11 14:48:05 -0700

    depth first tree search working

--- depth_first_tree.rb
+++ depth_first_tree.rb
@@ -10,6 +10,18 @@ class Tree
 
 end
 
+def find_node(node, search_value)
+  if node.payload == search_value
+    puts "returning #{node.payload}"
+    return node
+  end
+  node.children.each do |child|
+    puts "finding from child #{child.payload}"
+    checked = find_node(child, search_value)
+    return checked if checked.payload == search_value
+  end
+  node
+end
 
 # The "Leafs" of a tree, elements that have no children
 fifth_node    = Tree.new(5, [])
@@ -25,3 +37,5 @@ fifth_node = Tree.new(5, [ninth_node])
 
 # The "Trunk" of the tree
 trunk   = Tree.new(2, [seventh_node, fifth_node])
+
+puts find_node(trunk, 11).payload
