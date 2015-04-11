59165f0df7efc8e1043e5fce46edb04af48c1608
Jeremy Carey <jeremycar@gmail.com>
2015-04-11 16:07:49 -0700

    breadth first tree search working, could be drier

--- breadth_first_tree.rb
+++ breadth_first_tree.rb
@@ -7,9 +7,39 @@ class Tree
         @payload = payload
         @children = children
     end
-
 end
 
+class Queue
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(item)
+        @queue.push(item)
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def empty?
+      @queue.empty?
+    end
+end
+
+def find_value(trunk, search_value)
+  queue = Queue.new
+  trunk.children.each do |child|
+    return child if child.payload == search_value
+    queue.enqueue(child) unless child.children.empty?
+  end
+  until queue.empty?
+    queue.dequeue.children.each do |child|
+      return child if child.payload == search_value
+      queue.enqueue(child) unless child.children.empty?
+    end
+  end
+end
 
 # The "Leafs" of a tree, elements that have no children
 fifth_node    = Tree.new(5, [])
@@ -25,3 +55,5 @@ fifth_node = Tree.new(5, [ninth_node])
 
 # The "Trunk" of the tree
 trunk   = Tree.new(2, [seventh_node, fifth_node])
+
+puts find_value(trunk, 11).payload
